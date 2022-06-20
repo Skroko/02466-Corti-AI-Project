524426187b1adc404e0903383cccea82b7c1fe63
Rune-Nedergaard <rune7863@gmail.com>
2022-06-20 08:55:17 +0200

    bootstrapping

--- misc/text_to_phoneme.py
+++ misc/text_to_phoneme.py
@@ -10,10 +10,10 @@ from torch.utils.data import DataLoader
 from g2p_en import G2p
 from pypinyin import pinyin, Style
 
-#from utils.model import get_model, get_vocoder
-#from utils.tools import to_device, synth_samples
-#from dataset import TextDataset
-#from text import text_to_sequence
+from utils.model import get_model, get_vocoder
+from utils.tools import to_device, synth_samples
+from dataset import TextDataset
+from text import text_to_sequence
 from collections import defaultdict
 import nltk.data
 
@@ -63,23 +63,71 @@ def preprocess_english(text):
 
 # %%
 
-
 tokenizer = nltk.data.load('tokenizers/punkt/english.pickle',encoding="utf-8")
-fp = open("VCTK.txt",encoding="utf-8")
+fp = open("turing2.txt",encoding="utf-8")
 data = fp.read()
 
 # %%
+import jiwer
+transformation = jiwer.Compose([
+    jiwer.ToLowerCase(),
+    jiwer.RemovePunctuation(),
+    jiwer.ToLowerCase(),
+    jiwer.ExpandCommonEnglishContractions(),
+    jiwer.Strip(),
+    jiwer.RemoveWhiteSpace(replace_by_space=True),
+    #jiwer.RemoveMultipleSpaces(),
+    #jiwer.ReduceToListOfListOfWords(word_delimiter=" ")
+])
+
+
+
+#%%
+
 sentences = tokenizer.tokenize(data)
 sentences = [x.replace('\n',' ') for x in sentences]
 sentences = [x.replace('\xa0',' ') for x in sentences]
+sentences = [x.replace('\'','') for x in sentences]
+sentences = [x.replace('\"','') for x in sentences]
 
 #%%
-short_test = sentences[:10]
+flat = ''
+for i in range(len(sentences)):
+    flat = flat + ' ' + sentences[i]
+
+flat = transformation(flat)
+
 #%%
-# %%
+tokens = nltk.word_tokenize(flat)
+
+
+#%%
+########## FOR SPLITTING BASED ON TOKENS ##########
 df = defaultdict()
 index = 0
-for number, sentence in enumerate(short_test):
+index2 = 0
+current = ''
+for i in range(30):
+    current=''  
+    for u in range(7):
+        current = current + tokens[index]+ ' '
+        index += 1
+    print(current)
+    df[index2] = "|LJSpeech|"+preprocess_english(current)[0] +"|"+preprocess_english(current)[1]
+    index2 += 1
+  
+#%%
+with open('turing_phonemes_split.txt', 'a', encoding="utf-8") as f:
+    for key, value in df.items():
+        f.write(str(key)+value)
+        f.write('\n')
+
+
+# %%
+########## FOR SPLITTING LONGER SENTENCES ##########
+df = defaultdict()
+index = 0
+for number, sentence in enumerate(sentences):
     current = sentences[number].split()
     if len(current) > 10: #checks if sentence is too long, then splits and
         while len(current) > 10:
@@ -97,11 +145,28 @@ for number, sentence in enumerate(short_test):
         df[index] = "|LJSpeech|"+preprocess_english(sentence)[0] +"|"+preprocess_english(sentence)[1]
         index += 1
     print(df[number])
-
-# %%
-#Notice that it just continues writing if file already exists. its a feature, not a bug, definitely
-with open('VCTK_phoneme.txt', 'a') as f:
+with open('VCTK_phonemes_split.txt', 'a', encoding="utf-8") as f:
     for key, value in df.items():
         f.write(str(key)+value)
         f.write('\n')
+
+#%%
+######### WITHOUT SPLITTING LONGER SENTENCES ############
+sentences = sentences[949:] #starting from here, because fixd error
+
+#%%
+df = defaultdict()
+for index, sentence in enumerate(sentences):
+    df[index] = "|LJSpeech|"+preprocess_english(sentence)[0] +"|"+preprocess_english(sentence)[1] #+949 to resume building phoneme from where left of
+    print(df[index])
+
+with open('VCTK_phonemes.txt', 'a', encoding="utf-8") as f:
+    for key, value in df.items():
+        f.write(str(key)+value)
+        f.write('\n')
+
+
+# %%
+#Notice that it just continues writing if file already exists. its a feature, not a bug, definitely
+
 # %%
